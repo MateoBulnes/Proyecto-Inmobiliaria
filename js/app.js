9a3c0c3e01ca32cecf475a9fd59de98f678e94c9
MateoBulnes <mbulnes@fi.uba.ar>
2024-07-17 17:33:04 -0300

    Desarrollo navbar

---
+++ js/app.js
@@ -0,0 +1,16 @@
+const preloadImages = (sources) => {
+    sources.forEach((src) => {
+        const img = new Image();
+        img.src = src;
+    });
+};
+
+preloadImages(['/media/Logo_PGP-04.png', '/media/Logo_PGP-03.png']);
+
+window.addEventListener("scroll", () => {
+    let navbar = document.querySelector('#navbar');
+    let logo = document.querySelector('#img_logo_navbar img');
+
+    navbar.classList.toggle('fondo_navbar', window.scrollY > 0);
+    //(window.scrollY > 0) ? logo.src = '/media/Logo_PGP-04.png' : logo.src = '/media/Logo_PGP-03.png';
+});
