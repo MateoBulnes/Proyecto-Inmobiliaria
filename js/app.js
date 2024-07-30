c75325674c1933e81d176e8ed2b463d3723a427c
MateoBulnes <mbulnes@fi.uba.ar>
2024-07-30 14:14:24 -0300

    Termino páginas Contacto y Nosotros

--- js/app.js
+++ js/app.js
@@ -9,13 +9,21 @@ preloadImages(['/media/Logo_PGP-04.png', '/media/Logo_PGP-03.png']);
 
 window.addEventListener("scroll", () => {
     let navbar = document.querySelector('#navbar');
-    let logo = document.querySelector('#img_logo_navbar img');
+    let logo_base = document.querySelector('#navbar .logo_base');
+    let logo_scroll = document.querySelector('#navbar .logo_scroll');
+
+    if (window.scrollY > 0) {
+        logo_base.style.display = 'none';
+        logo_scroll.style.display = '';
+    } else {
+        logo_base.style.display = '';
+        logo_scroll.style.display = 'none';
+    }
 
     navbar.classList.toggle('fondo_navbar', window.scrollY > 0);
-    //(window.scrollY > 0) ? logo.src = '/media/Logo_PGP-04.png' : logo.src = '/media/Logo_PGP-03.png';
 });
 
 document.addEventListener("DOMContentLoaded", function () {
-    /*var video = document.getElementById("video_nosotros");
-    video.play();*/
+    let video = document.getElementById("video_nosotros");
+    video.play();
 });
