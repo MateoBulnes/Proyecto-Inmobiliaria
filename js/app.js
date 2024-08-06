f1c398b9dc942484f03aef86251719d132bc560f
MateoBulnes <mbulnes@fi.uba.ar>
2024-08-06 15:46:14 -0300

    Fix: color navbar + agrego redes en footer

--- js/app.js
+++ js/app.js
@@ -1,26 +1,42 @@
-const preloadImages = (sources) => {
-    sources.forEach((src) => {
-        const img = new Image();
-        img.src = src;
-    });
+/*API*/
+//const API_KEY = '5940ea45eb7cfb55228bec0b958ea9c0be151757';
+/*const API_KEY = 'de8bf11f2f631662a2b561aa688c0efa1764a6ff';
+const categoria = 'location';
+var id_propiedad = 24880;
+const url = `https://www.tokkobroker.com/api/v1/${categoria}/${id_propiedad}/?key=${API_KEY}&lang=es_ar&format=json`;
+
+const options = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: 'API_KEY'
+    }
 };
 
-preloadImages(['/media/Logo_PGP-04.png', '/media/Logo_PGP-03.png']);
+fetch(url, options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+*/
+var pagina_actual = window.location.pathname.split('/').pop();
 
 window.addEventListener("scroll", () => {
     let navbar = document.querySelector('#navbar');
     let logo_base = document.querySelector('#navbar .logo_base');
     let logo_scroll = document.querySelector('#navbar .logo_scroll');
 
-    if (window.scrollY > 0) {
-        logo_base.style.display = 'none';
-        logo_scroll.style.display = '';
-    } else {
-        logo_base.style.display = '';
-        logo_scroll.style.display = 'none';
-    }
+    if (pagina_actual == 'index.html') {
+        if (window.scrollY > 0) {
+            logo_base.style.display = 'none';
+            logo_scroll.style.display = '';
+        } else {
+            logo_base.style.display = '';
+            logo_scroll.style.display = 'none';
+        }
 
-    navbar.classList.toggle('fondo_navbar', window.scrollY > 0);
+        navbar.classList.toggle('fondo_navbar', window.scrollY > 0);
+    }
 });
 
 document.addEventListener("DOMContentLoaded", function () {
